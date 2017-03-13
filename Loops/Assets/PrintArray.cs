7280375a714bcd5acf8961280ebec7f64e6b46f1
Robert Svärdström <svardstromr@gmail.com>
2017-03-13 12:44:07 +0100

    Made a loop for printing out a list.

---
+++ Loops/Assets/PrintArray.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrintArray : MonoBehaviour {
+    List<int> list = new List<int>();   // Use this for initialization
+
+    void Start ()
+    {   //Adds to list.
+        list.Add(1);
+        list.Add(7);
+        list.Add(69);
+        list.Add(420);
+        list.Add(666);
+        list.Add(999);
+        //makes a loop while i is less than the list length.
+        for (int i = 0; i < list.Count; i++)
+        {
+            print(list[i]);
+        }
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
