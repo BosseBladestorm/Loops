1cef2cbe46c212191d26bf642777d730e8170372
Robert Svärdström <svardstromr@gmail.com>
2017-03-27 11:05:41 +0200

    Made a ArraySearch

---
+++ Loops/Assets/SearchArray.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SearchArray : MonoBehaviour {
+    List<int> list = new List<int>();   // Use this for initialization
+    public int compare;
+
+    void Start()
+    {   //Adds to list.
+        list.Add(1);
+        list.Add(7);
+        list.Add(69);
+        list.Add(420);
+        list.Add(666);
+        list.Add(999);
+        //makes a loop while i is less than the list length.
+        for (int i = 0; i < list.Count; i++)
+        {
+            if (list[i] == compare)
+            {
+                print(i);
+            }
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
